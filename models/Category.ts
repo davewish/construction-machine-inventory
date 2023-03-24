bcdc0acd087e023381224fa09ac03c589d2568a6
dawit <dvwish9214@gmail.com>
2023-03-24 12:10:52 +0300

    contruction machine inventory app started

---
+++ models/Category.ts
@@ -0,0 +1,9 @@
+class MachineCategory {
+  categoryId: string;
+  categoryName: string;
+
+  constructor(categoryId: string, categoryName: string) {
+    this.categoryId = categoryId;
+    this.categoryName = categoryName;
+  }
+}
